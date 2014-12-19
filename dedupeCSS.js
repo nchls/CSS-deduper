a9c202f1eddb4547194dfdf77c5b79425922efc7
Ian Nicholas <inicholas@llbean.com>
2014-12-18 20:09:21 -0500

    Combining sheets

--- dedupeCSS.js
+++ dedupeCSS.js
@@ -5,9 +5,9 @@ var _ = require('lodash'),
 
 var tabletFlags = [
         '.tablet',
+        '.tablet.rd-rollover-nav',
         '.rd-responsive-page'
     ],
-    tabletFlagsLength = tabletFlags.length,
     desktopSheets = [
         '/input/global.min.css',
         '/input/gnglobal.min.css'
@@ -16,7 +16,10 @@ var tabletFlags = [
 var init = function(callback) {
     var stylesheets = ['/input/tablet.min.css'].concat(desktopSheets);
 
-    q.all(_.map(stylesheets, parseSheet)).then(trawlSheets);
+    q.all(_.map(stylesheets, parseSheet)).then(function(responses) {
+        combinedAst = trawlSheets(responses);
+        console.log(css.stringify(combinedAst));
+    });
 };
 
 var parseSheet = function(path) {
@@ -27,23 +30,9 @@ var parseSheet = function(path) {
             deferred.reject(err);
         }
 
-        var output = [],
-            ast = css.parse(content.toString());
+        var ast = css.parse(content.toString());
 
-        _.forEach(ast, function(item) {
-            if (item.rules) {
-                _.forEach(item.rules, function(node) {
-                    if (node.type === 'rule') {
-                        output.push([
-                            node.selectors,
-                            node.declarations
-                        ]);
-                    }
-                });
-            }
-        });
-
-        deferred.resolve(output);
+        deferred.resolve(ast);
     });
 
     return deferred.promise;
@@ -52,63 +41,126 @@ var parseSheet = function(path) {
 var trawlSheets = function(responses) {
     var tabletSheet = responses[0],
         desktopSheets = responses.slice(1),
-        dSheetsLength = desktopSheets.length;
-
-    for (var ruleIndex=0, l=tabletSheet.length; ruleIndex<l; ruleIndex++) {
-        var tRule = tabletSheet[ruleIndex],
-            tSelectors = tRule[0],
-            tDeclarations = tRule[1];
-
-        for (var i=0, l2=tSelectors.length; i<l2; i++) {
-            var tSelector = tSelectors[i];
-            for (var j=0; j<tabletFlagsLength; j++) {
-                tSelector = tSelector.replace(tabletFlags[j] + ' ', '');
+        combinedAst = {
+            type: 'stylesheet',
+            stylesheet: {
+                rules: []
             }
+        },
+        combinedAstKeys = [];
 
-            for (var dSheetIdx=0; dSheetIdx<dSheetsLength; dSheetIdx++) {
-                var desktopSheet = desktopSheets[dSheetIdx];
-
-                for (var j=0, l3=desktopSheet.length; j<l3; j++) {
-                    var dRule = desktopSheet[j],
-                        dSelectors = dRule[0],
-                        dDeclarations = dRule[1];
-
-                    for (var k=0, l4=dSelectors.length; k<l4; k++) {
-                        var dSelector = dSelectors[k];
-                        if (tSelector === dSelector) {
-
-                            for (var m=0, l5=tDeclarations.length; m<l5; m++) {
-                                var tDeclaration = tDeclarations[m],
-                                    tProperty,
-                                    tValue;
-                                
-                                if (tDeclaration.type === 'declaration') {
-                                    tProperty = tDeclaration.property;
-                                    tValue = tDeclaration.value;
-                                    
-                                    for (var n=0, l6=dDeclarations.length; n<l6; n++) {
-                                        var dDeclaration = dDeclarations[n],
-                                            dProperty,
-                                            dValue;
-                                    
-                                        if (dDeclaration.type === 'declaration') {
-                                            dProperty = dDeclaration.property;
-                                            dValue = dDeclaration.value;
-                                    
-                                            if (dProperty === tProperty) {
-                                                console.log(tSelector);
-                                                console.log('\t' + dProperty + ': ' + dValue + '; is overridden by ' + tProperty + ': ' + tValue + ';');
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+    var addNode = _.memoize(function(node) {
+        var selectorsKey = node.selectors.join(',');
+        if (combinedAstKeys.indexOf(selectorsKey) === -1) {
+            combinedAst.stylesheet.rules.push(node);
+            combinedAstKeys.push(selectorsKey);
+        } else {
+            var dupeRule = _.find(combinedAst.stylesheet.rules, {selectors: node.selectors});
+            _.forEach(node.declarations, function(declaration) {
+                var dupeDeclaration = _.find(dupeRule.declarations, {property: declaration.property});
+                if (!dupeDeclaration) {
+                    // Same selectors, but new property -- add it to the AST
+                    dupeRule.declarations.push(declaration);
                 }
-            }
+            });
         }
-    }
+    }, JSON.stringify);
+
+    _.forEach(tabletSheet, function(tItem) {
+
+        if (tItem.rules) {
+
+            _.forEach(tItem.rules, function(tNode) {
+
+                var tNodeCandidate = tNode,
+                    tSelectors = tNode.selectors,
+                    tDeclarations = tNode.declarations;
+
+                if (tNode.type === 'rule') {
+
+                    _.forEach(tSelectors, function(tSelector) {
+
+                        var foundMatchingSelector = false;
+
+                        _.forEach(tabletFlags, function(flag) {
+                            tSelector = tSelector.replace(flag + ' ', '');
+                        });
+
+                        _.forEach(desktopSheets, function(desktopSheet) {
+
+                            _.forEach(desktopSheet, function(dItem) {
+
+                                if (dItem.rules) {
+
+                                    _.forEach(dItem.rules, function(dNode) {
+
+                                        var dNodeCandidate = _.cloneDeep(dNode),
+                                            dSelectors = dNode.selectors,
+                                            dDeclarations = dNode.declarations;
+
+                                        if (dNode.type === 'rule') {
+
+                                            _.forEach(dSelectors, function(dSelector) {
+
+                                                if (tSelector === dSelector) {
+
+                                                    foundMatchingSelector = true;
+
+                                                    // Now that we have a selector match, find any identical declarations
+                                                    _.forEach(tDeclarations, function(tDeclaration) {
+
+                                                        if (tDeclaration.type === 'declaration') {
+                                                            
+                                                            var tProperty = tDeclaration.property;
+
+                                                            _.forEach(dDeclarations, function(dDeclaration) {
+
+                                                                if (dDeclaration.type === 'declaration') {
+
+                                                                    var dProperty = dDeclaration.property;
+
+                                                                    if (dProperty === tProperty) {
+
+                                                                        var candidateDeclaration = _.find(dNodeCandidate.declarations, {property: dProperty});
+                                                                        _.pull(dNodeCandidate.declarations, candidateDeclaration);
+                                                                        if (!dNodeCandidate.declarations.length) {
+                                                                            dNodeCandidate = null;
+                                                                        }
+
+                                                                    }
+                                                                }
+                                                            });
+                                                        }
+                                                    });
+                                                }
+                                            });
+
+                                            // Add the desktop rule after removing any tablet-overridden declarations
+                                            if (dNodeCandidate) {
+                                                addNode(dNodeCandidate);
+                                            }
+
+                                        }
+                                    });
+                                }
+                            });
+                        });
+
+                        // Tablet selector was not found in desktop CSS -- so just add it to the combined
+                        if (!foundMatchingSelector) {
+                            addNode(tNodeCandidate);
+                        }
+
+                        desktopPassCompleted = true;
+
+                    }); 
+                }
+            });
+        }
+    });
+
+    return combinedAst;
+
 };
 
 init();
